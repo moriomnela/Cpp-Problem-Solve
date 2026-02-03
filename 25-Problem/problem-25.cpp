894c827a35f9a56893a93ef5d2f047c73fcccf17
moriomnela <moriomislamnela23@gmail.com>
2026-02-04 00:16:43 +0600

    Cpp Problem solve 25

---
+++ 25-Problem/problem-25.cpp
@@ -0,0 +1,22 @@
+/*Print multiplication table*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+   int number;
+
+   cout << "Enter number: ";
+   cin >> number;
+
+    int n = 1;
+    int multiplication = 0;
+   while(n <= 10){
+       multiplication = number * n;
+       cout << number << "*" << n << "=" << multiplication <<endl;
+       n++;
+     }
+  return 0;
+}
+
