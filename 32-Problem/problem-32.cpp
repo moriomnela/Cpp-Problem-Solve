827340a97451f48b5ef5478c4145b5eca2eaca6f
moriomnela <moriomislamnela23@gmail.com>
2026-02-22 16:44:48 +0600

    Print all prime numbers between two numbers

--- 32-Problem/problem-32.cpp
+++ 32-Problem/problem-32.cpp
@@ -13,16 +13,20 @@ int main(){
 
     int num;
 
-    bool isprime = true;
     for(int num = startnumber;num <= endnumber; num++){
+       bool isprime = true;
          for(int i=2; i*i <= num; i++){
-        if(num%i==0){
-            isprime=false;
-            break;
-        }
-      }
-       cout << "Prime Number is: " << num << endl;
+            if(num%i==0){
+                isprime=false;
+                break;
+            }
+
+       }
+       if(isprime == true){
+           cout << "Prime Number is: " << num << endl;
+       }
     }
+
 
 return 0;
 }
