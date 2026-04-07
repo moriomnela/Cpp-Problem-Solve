9beb6f3faae447cd062ec9d2a204ad6266d6b656
moriomnela <moriomislamnela23@gmail.com>
2026-04-07 17:53:25 +0600

    Prime number

--- 32-Problem/problem-32.cpp
+++ 32-Problem/problem-32.cpp
@@ -1,32 +1,32 @@
-/*Check prime number*/
+/*Print all prime numbers between two numbers*/
+
 #include <iostream>
 using namespace std;
 int main(){
 
     int startnumber;
-    cout << "Enter number:" ;
+    cout << "Enter 1st number:" ;
     cin >> startnumber;
 
     int endnumber;
-    cout << "Enter number:" ;
+    cout << "Enter 2nd number:" ;
     cin >> endnumber;
 
-    int num;
-
     for(int num = startnumber;num <= endnumber; num++){
-       bool isprime = true;
-         for(int i=2; i*i <= num; i++){
-            if(num%i==0){
-                isprime=false;
-                break;
+          if(num < 2){
+            continue;
             }
 
+      bool isprime = true;
+         for(int i=2; i*i <= num; ++i){
+            if(num%i==0){
+                isprime = false;
+                break;
+            }
        }
-       if(isprime == true){
-           cout << "Prime Number is: " << num << endl;
-       }
+    if(isprime)
+        cout << "Prime Number is: " << num << endl;
     }
-
 
 return 0;
 }
